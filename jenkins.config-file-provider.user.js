def5afeec41cac83f90ed5f23508c570c01f5f97
KARASZI István <github@spam.raszi.hu>
2016-04-28 13:11:16 +0200

    Add Jenkins Config File Provider style fix

---
+++ jenkins.config-file-provider.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        Jenkins Config File Provider Style
+// @namespace   http://userscripts.org/users/20715
+// @description Customize the appearance of the config editor
+// @include     http://jenkins*/*
+// @grant       GM_addStyle
+// @version     1.0
+// ==/UserScript==
+GM_addStyle("body[data-model-type='org.jenkinsci.plugins.configfiles.ConfigFilesManagement'] textarea[id='config.content'] { \
+	font-family: monaco, Consolas, Lucida Console, monospace; \
+  font-size: 12px; \
+}");
