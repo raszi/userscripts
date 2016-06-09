323d423768679d9a0eee3f04b8731f38f190735a
KARASZI István <github@spam.raszi.hu>
2016-06-09 12:38:33 +0200

    Remove jQuery reference

--- github.lgtm.user.js
+++ github.lgtm.user.js
@@ -3,32 +3,29 @@
 // @namespace   http://userscripts.org/users/20715
 // @description Adds a LGTM button to the code-review sreen.
 // @include     https://github*/*
-// @version     1.1
+// @version     1.2
 // ==/UserScript==
-(function ($) {
-  function addButton() {
-    var form = document.querySelector('form.js-new-comment-form');
+function addButton() {
+  var form = document.querySelector('form.js-new-comment-form');
 
-    if (!form || form.querySelector('.btn-lgtm')) {
-      return;
-    }
-
-    var textarea = form.querySelector('textarea'),
-        button = form.querySelector('button.btn-primary'),
-        lgtmButton = button.cloneNode(true);
-
-    lgtmButton.textContent = 'LGTM';
-    lgtmButton.classList.add('btn-secondary', 'btn-lgtm');
-    lgtmButton.classList.remove('btn-primary');
-    lgtmButton.addEventListener('click', function () {
-      textarea.value = 'LGTM';
-      button.click();
-    });
-
-    button.parentNode.appendChild(lgtmButton);
+  if (!form || form.querySelector('.btn-lgtm')) {
+    return;
   }
 
-  addButton();
+  var textarea = form.querySelector('textarea'),
+      button = form.querySelector('button.btn-primary'),
+      lgtmButton = button.cloneNode(true);
 
-  document.addEventListener('pjax:success', addButton);
-}(jQuery));
+  lgtmButton.textContent = 'LGTM';
+  lgtmButton.classList.add('btn-secondary', 'btn-lgtm');
+  lgtmButton.classList.remove('btn-primary');
+  lgtmButton.addEventListener('click', function () {
+    textarea.value = 'LGTM';
+    button.click();
+  });
+
+  button.parentNode.appendChild(lgtmButton);
+}
+
+document.addEventListener('DOMContentLoaded', addButton);
+document.addEventListener('pjax:success', addButton);
