4f4410bbca116f357707f37fbdf5a3359ae5887d
KARASZI István <github@spam.raszi.hu>
2016-09-13 16:34:53 +0200

    Hide submenu

--- grafana.toolbar.hide.user.js
+++ grafana.toolbar.hide.user.js
@@ -4,9 +4,10 @@
 // @description   Hides the Grafana toolbar
 // @include       https://grafana*
 // @grant         GM_addStyle
-// @version       1.2
+// @version       1.3
 // ==/UserScript==
 
 GM_addStyle('.navbar { display: none; }');
 GM_addStyle('.row-control-inner { display: none; }');
 GM_addStyle('.add-row-panel-hint { display: none; }');
+GM_addStyle('dashboard-submenu { display: none; }');
