360ca0d7f9c2e15895e48926e898157ca0dd446b
KARASZI István <github@spam.raszi.hu>
2017-07-10 12:27:05 +0200

    Add NodePing style fixes

---
+++ nodeping.style.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         NodePing Style Fixes
+// @namespace    http://userscripts.org/users/20715
+// @version      0.1
+// @description  Various NodePing style fixes.
+// @match        https://nodeping.com/
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    GM_addStyle('.ui-dialog { box-shadow: 0px 0px 20px -2px rgba(0,0,0,0.75); }');
+})();
