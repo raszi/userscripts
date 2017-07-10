145a9a4d3c0ed333eb3a3824cbaaaa443b3a3b2e
KARASZI István <github@spam.raszi.hu>
2017-07-10 12:33:06 +0200

    Fix whitespaces

--- nodeping.style.user.js
+++ nodeping.style.user.js
@@ -8,6 +8,6 @@
 // ==/UserScript==
 
 (function() {
-    'use strict';
-    GM_addStyle('.ui-dialog { box-shadow: 0px 0px 20px -2px rgba(0,0,0,0.75); }');
+  'use strict';
+  GM_addStyle('.ui-dialog { box-shadow: 0px 0px 20px -2px rgba(0,0,0,0.75); }');
 })();
