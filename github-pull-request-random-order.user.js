766a929f86c716bea18ce72efa29b78d96bb2d82
KARASZI István <ikaraszi@gmail.com>
2022-05-24 21:20:56 +0200

    Random the file order in a GitHub PR

---
+++ github-pull-request-random-order.user.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name         GitHub Code Review Random Order
+// @namespace    https://github.com/raszi/userscripts
+// @version      0.1
+// @description  Randomizes the order of the files in a code review
+// @author       István Karaszi
+// @match        https://github.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
+// @grant        none
+// ==/UserScript==
+
+(() => {
+  'use strict';
+
+  const $x = (xp, node = document) => {
+    const snapshot = document.evaluate(xp, node, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+
+    return [...Array(snapshot.snapshotLength)].map((_, i) => snapshot.snapshotItem(i));
+  };
+
+  const randomizeFileOrder = () => {
+    console.log('Randomize');
+
+    const [container] = $x('//*[contains(@class, "js-diff-progressive-container")]');
+
+    if (!container || container.classList.contains('randomized')) {
+      console.log('Already randomized');
+      return;
+    }
+
+    $x('*[contains(@class, "js-file")]', container).forEach((_node, i) => {
+      container.appendChild(container.children[(Math.random() * i) | 0]);
+    });
+
+    container.classList.add('randomized');
+  };
+
+  randomizeFileOrder();
+
+  console.log('Run');
+  document.addEventListener('pjax:end', randomizeFileOrder);
+})();
